07e620be45ac80967fc63e05cceb9a6e903cd3d9
Rifki02 <44845664+Rifki02@users.noreply.github.com>
2023-10-29 16:46:01 +0800

    Register.php

--- Posttest6/Register.php
+++ Posttest6/Register.php
@@ -3,82 +3,39 @@
 require "database/koneksi.php";
 session_start();
 
-$id = null; // Initialize $id to null
-
-if (isset($_GET["id"])) {
-    $id = $_GET["id"];
-} else {
-    // Handle the case where 'id' is not set, perhaps by displaying an error message or redirecting.
-}
-
-$result = mysqli_query($conn, "SELECT * FROM informasi WHERE id='$id'");
-
-$informasi = [];
-
-if (!empty($result)) {
-    while ($row = mysqli_fetch_assoc($result)) {
-        $informasi[] = $row;
-    }
-    if (!empty($informasi)) {
-        $informasi = $informasi[0];
-    } else {
-        // Handle the case where the array is empty, perhaps by displaying an error message.
-    }
-} else {
-    // Handle the case where the query fails, perhaps by displaying an error message.
-}
-
 if (isset($_POST["Register"])) {
     $nama = $_POST["nama"];
     $alamat = $_POST["alamat"];  // Corrected the input name
     $telephone = $_POST["telephone"];  // Corrected the input name
     $password = $_POST["password"];
-    $foto = $_FILES['foto']['name'];
-    $x = explode('.', $foto);
-    $ekstensi = strtolower(end($x));
+    $foto = $_FILES["gambar"]["name"];
+    $explode = explode('.', $foto);
+    $ekstensi = strtolower(end($explode));
+    $gambar = date("Y-m-d"). $foto;
+    $temp = $_FILES['gambar']['tmp_name'];
 
-    $new_foto = "$nama.$ekstensi";
-    $tmp = $_FILES['foto']['tmp_name'];
 
-    // Destination directory for file uploads (make sure it exists and is writable)
-    $destination_directory = "../Resource/";  // Adjust the path to the correct directory
+    if (move_uploaded_file($temp,'Resource/' . $gambar)) {
+        $result = mysqli_query($conn, "INSERT into informasi(id, nama, alamat, telephone, password, gambar)
+        values('', '$nama', '$alamat', '$telephone', '$password' , '$gambar') ");
 
-    if (file_exists($destination_directory) && is_writable($destination_directory)) {
-        $new_file_path = $destination_directory . $new_foto;
-    }
-        if (move_uploaded_file($tmp, $new_file_path)) {
-            // The file was successfully moved.
-
-            $get_foto = mysqli_query($conn, "SELECT foto FROM informasi WHERE id = $id");
-            $data_old = mysqli_fetch_array($get_foto);
-            unlink("../img/".$data_old['foto']);
-
-            $result = mysqli_query($conn, "UPDATE informasi SET nama='$nama', alamat='$alamat', telephone='$telephone', password='$password', foto='$new_foto' WHERE id = $id");
-
-            if ($result) {
-                $last_id = $conn->insert_id;
-                $_SESSION["id"] = $last_id;
-                echo "
-                    <script>
+        if ($result) {
+            echo "
+                <script>
                     alert('Data Berhasil Ditambahkan!');
                     document.location.href = 'index.html';
-                    </script>
-                ";
-            } else {
-                echo "
-                    <script>
-                    alert('Data Gagal Ditambahkan!');
-                    document.location.href = 'Register.php';
-                    </script>
+                </script>
+            ";
+        } else {
+            echo "
+                <script>
+                alert('Data Gagal Ditambahkan!');
+                document.location.href = 'Register.php';
+                </script>
                 ";
             }
-        } else {
-            echo "Error: Failed to move the uploaded file.";
         }
-//     } else {
-//         echo "Error: Destination directory doesn't exist or is not writable.";
-//     }
-// }
+    }
 ?>
 
 <!-- Your HTML form with corrected labels and input names -->
@@ -115,7 +72,7 @@ if (isset($_POST["Register"])) {
                         <input type="password" name="password" maxlength="50" placeholder="password"/>
                     </div>
                     <div class="inputBox">
-                        <input type="file" name="foto" class="textfield">
+                        <input type="file" name="gambar" class="textfield">
                     </div>
                     <div class="inputBox">
                         <button type="submit" name="Register" id="Register">Register</button>
